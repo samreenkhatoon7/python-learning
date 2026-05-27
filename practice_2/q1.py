ea4af4601063326417a8a4090d7545c8b3257fe8
samreenkhatoon7 <samreenkhatoon81@gmail.com>
2026-05-27 11:03:16 +0530

    update

--- practice_2/q1.py
+++ practice_2/q1.py
@@ -1,1 +1,3 @@
 # sum of 3 digit
+# whar hggcgfdg
+bnbhkkjh
---
+++ practice_set_1/Q_1.py
@@ -0,0 +1,4 @@
+# Write a program that will convert celsius value to kelvin
+celsius= float(input("enter of temperature in celsius:"))
+kelvin = celsius + 273.15
+print(kelvin)
